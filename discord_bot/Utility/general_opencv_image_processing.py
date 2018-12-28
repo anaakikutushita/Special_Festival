90a2d271795f998af7b9c9398a5c8d063ba56602
あつし <atushi_anaaki@yahoo.co.jp>
2018-12-28 17:26:40 +0900

    数字認識部分のテストを書いた

---
+++ discord_bot/Utility/general_opencv_image_processing.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""画像処理をするときに汎用的に使えるコードをまとめたモジュール"""
+
+import glob
+import cv2
+
+class AllJpgImageImporterInsideFolder():
+    """特定のフォルダ内に存在する全ての画像ファイルのフルパスを取得する"""
+    def __init__(self, image_folder_path):
+        #あとで、フォルダの区切り文字とか末尾の文字を揃える処理が必要になるはず
+        #今のところは「スラッシュ1文字区切り」「フォルダパスの最後にスラッシュが入っている」条件で決め打ちする
+        self._image_folder_path = image_folder_path
+
+    def get_image_list_jpg(self):
+        """フォルダ内の.jpg拡張子のみ取得"""
+        return self._get_image_list("*.jpg")
+
+    def get_image_list_png(self):
+        """フォルダ内の.png拡張子のみ取得"""
+        return self._get_image_list("*.png")
+
+    def _get_image_list(self, ext):
+        """
+        拡張子が一致する画像ファイルのフルパスのリストを返す。
+        extには "*.jpg" などを指定する。
+        "*.*" を指定した場合、 jpg, png, gif, bmp, ico, svg, tiff の7種まで対応
+        """
+        image_file_path_list = glob.glob(self._image_folder_path + ext)
+
+        img_list = []
+        for image_file_path in image_file_path_list:
+            orig_img = cv2.imread(image_file_path)
+            img_list.append(orig_img)
+
+        return img_list
--- discord_bot/unit_test.py
+++ discord_bot/unit_test.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 import unittest
 import cv2
 
 import process_image
+from Utility.general_opencv_image_processing import AllJpgImageImporterInsideFolder
 
 class Test_ImageResizer(unittest.TestCase):
     def setUp(self):
@@ -25,7 +27,54 @@ class Test_ImageResizer(unittest.TestCase):
 class Test_UsingTimesCalclator(unittest.TestCase):
     def test_get_number_inside_image(self):
         """#4 ~ #7"""
-        
+
+        # 4
+        expected_num = 3
+        img4 = cv2.imread("unittest_resource/Issues/4/region1.jpg")
+        calclator = process_image.UsingTimesCalclator(img4)
+        num = calclator._get_number_inside_image(img4)
+        self.assertEqual(num, expected_num)
+
+        # 5
+        expected_num = [x for x in range(11)]
+        importer = AllJpgImageImporterInsideFolder("unittest_resource/Issues/5")
+        img_list5 = importer.get_image_list_jpg()
+        num_list = []
+        for img5 in img_list5:
+            num = calclator._get_number_inside_image(img5)
+            num_list.append(num)
+        print(num_list)
+        print(expected_num)
+        for var in range(0, 11):
+            self.assertEqual(num_list[var], expected_num[var])
+
+        # 6
+        expected_num = 0
+        img6 = cv2.imread("unittest_resource/Issues/6/0.jpg")
+        calclator = process_image.UsingTimesCalclator(img6)
+        num = calclator._get_number_inside_image(img6)
+        self.assertEqual(num, expected_num)
+
+        # 7
+        expected_num = 6+4+7+2+1+10+3+3
+        importer = AllJpgImageImporterInsideFolder("unittest_resource/Issues/7")
+        img_list7 = importer.get_image_list_jpg()
+        using_times = 0
+        for img7 in img_list7:
+            num = calclator._get_number_inside_image(img7)
+            using_times += num
+        self.assertEqual(using_times, expected_num)
+
+        # 8
+        expected_num = 4+0+0+0+9+0+0+0
+        importer = AllJpgImageImporterInsideFolder("unittest_resource/Issues/8")
+        img_list8 = importer.get_image_list_jpg()
+        using_times = 0
+        for img8 in img_list8:
+            num = calclator._get_number_inside_image(img8)
+            using_times += num
+        self.assertEqual(using_times, expected_num)
+
 
 if __name__ == '__main__':
     unittest.main()
